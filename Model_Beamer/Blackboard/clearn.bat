33eb254a75ad4e294001503f5bf795139e16940f
molakirlee <molakirlee@sina.com>
2019-07-20 21:31:35 +0800

    update

---
+++ Model_Beamer/Blackboard/clearn.bat
@@ -0,0 +1,14 @@
+del *.aux
+del *.bbl
+del *.blg
+del *.log
+del *.nav
+del *.out
+del *.run.xml
+del *.snm
+del *.synctex.*
+del *.toc
+del *-blx.bib
+del *.vrb
+del *.bcf
+del *.pdf
---
+++ Model_Beamer/Blackboard/genPPT.cmd
@@ -0,0 +1,4 @@
+xelatex -output-driver="xdvipdfmx -V 5" kmbeamer_bg_Blackboard.tex
+rem xelatex -output-driver="xdvipdfmx -V 5" main.tex
+rem bibtex main.aux
+xelatex -output-driver="xdvipdfmx -V 5" main.tex
