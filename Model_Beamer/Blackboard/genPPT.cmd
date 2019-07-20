4681ec8bdcedd5cf498c4197ca2db613cf35ab92
molakirlee <molakirlee@sina.com>
2019-07-20 22:17:54 +0800

    update

--- Model_Beamer/Blackboard/genPPT.cmd
+++ Model_Beamer/Blackboard/genPPT.cmd
@@ -2,3 +2,4 @@ xelatex -output-driver="xdvipdfmx -V 5" kmbeamer_bg_Blackboard.tex
 rem xelatex -output-driver="xdvipdfmx -V 5" main.tex
 rem bibtex main.aux
 xelatex -output-driver="xdvipdfmx -V 5" main.tex
+xelatex -output-driver="xdvipdfmx -V 5" main.tex
